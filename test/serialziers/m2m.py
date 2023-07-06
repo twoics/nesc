fba822d01d1636ad90924a9a25b3ef3de21d816a
twoics <second_vozhzhov@mail.ru>
2023-07-07 00:21:49 +0700

    feat: add test app

---
+++ test/serialziers/m2m.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+
+from ..models.m2m import Article, Publication
+from serializer_service.core.services import SerializerCreateService
+
+
+class PublicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Publication
+        fields = (
+            'id',
+            'title',
+        )
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    publications = PublicationSerializer(
+        many=True
+    )
+
+    class Meta:
+        model = Article
+        fields = (
+            'id',
+            'headline',
+            'publications',
+        )
+
+    def create(self, validated_data):
+        self.context['raw_data'] = validated_data.copy()
+        validated_data.pop('publications')
+
+        current_instance = super().create(validated_data)
+        service = SerializerCreateService(self, validated_data, m2m_fields=['publications'])
+
+        service.create_m2m_instances(
+            related_name='publications',
+            parent_instance=current_instance
+        )
+        return current_instance
+
