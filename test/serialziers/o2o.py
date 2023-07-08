020f2035dde7681ffa08bbc31fbdfa36172c5e67
twoics <second_vozhzhov@mail.ru>
2023-07-08 18:56:50 +0700

    fix: fix serialzeir by new relation

--- test/serialziers/o2o.py
+++ test/serialziers/o2o.py
@@ -1,21 +1,20 @@
 from rest_framework import serializers
 
-from ..models.o2o import Place, Restaurant
+from ..models.o2o import RestaurantDirector, Restaurant
 from nesc.nesc.service import SerializerCreateService
 
 
-class PlaceSerializer(serializers.ModelSerializer):
+class RestaurantDirectorSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Place
+        model = RestaurantDirector
         fields = (
             'id',
             'name',
-            'address',
         )
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
-    place = PlaceSerializer(
+    director = RestaurantDirectorSerializer(
         required=True
     )
 
@@ -23,16 +22,18 @@ class RestaurantSerializer(serializers.ModelSerializer):
         model = Restaurant
         fields = (
             'id',
-            'place',
             'serves_hot_dogs',
             'serves_pizza',
+            'director',
         )
 
     def create(self, validated_data):
-        data_without_place = validated_data.copy()
-        data_without_place.pop('place')
-        restaurant = super().create(data_without_place)
+        service = SerializerCreateService(
+            self,
+            validated_data,
+            child_fields=['director']
+        )
+        restaurant = super().create(validated_data)
 
-        service = SerializerCreateService(self, validated_data, child_fields=['place'])
         service.create_child_instances(restaurant)
         return restaurant
