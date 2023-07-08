8fcc906965556d152d7a0de2db0f510f003e5443
twoics <second_vozhzhov@mail.ru>
2023-07-08 16:14:49 +0700

    feat: add fk model

--- test/models/m2m.py
+++ test/models/m2m.py
@@ -2,14 +2,18 @@ from django.db import models
 
 
 class Publication(models.Model):
-    title = models.CharField(max_length=30)
+    title = models.CharField(
+        max_length=30
+    )
 
     def __str__(self):
         return self.title
 
 
 class Article(models.Model):
-    headline = models.CharField(max_length=100)
+    headline = models.CharField(
+        max_length=100
+    )
     publications = models.ManyToManyField(
         Publication,
         related_name="articles"
