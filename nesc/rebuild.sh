c4f5d90dd2f0e2a5871d0dff13753f0fb187a119
twoics <second_vozhzhov@mail.ru>
2023-07-09 13:09:17 +0700

    ci: add ls to nesc

--- nesc/rebuild.sh
+++ nesc/rebuild.sh
@@ -2,7 +2,7 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
 cd "$parent_path" || exit
 
-ls -a
+ls ls nesc/ -a
 #rm -rf build/
 #rm -rf dist/
 #rm -rf serializer_service.egg-info/
