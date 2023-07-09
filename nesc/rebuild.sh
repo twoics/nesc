699b12107e4c847f1b5aed67149b09801fd8ef37
twoics <second_vozhzhov@mail.ru>
2023-07-09 13:04:47 +0700

    ci: add ls to rebuild

--- nesc/rebuild.sh
+++ nesc/rebuild.sh
@@ -2,11 +2,12 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
 cd "$parent_path" || exit
 
-rm -rf build/
-rm -rf dist/
-rm -rf serializer_service.egg-info/
-
-python setup.py sdist
-python setup.py bdist_wheel
-
-twine check dist/*
+ls -a
+#rm -rf build/
+#rm -rf dist/
+#rm -rf serializer_service.egg-info/
+#
+#python setup.py sdist
+#python setup.py bdist_wheel
+#
+#twine check dist/*
